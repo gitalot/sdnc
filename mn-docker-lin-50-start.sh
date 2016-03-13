fb821b965ae38a4a792682a8b4770b164c6ebaac
Kalidas Porika <kporika@gmail.com>
2016-03-13 13:24:36 -0400

    added modprobe

--- mn-docker-lin-50-start.sh
+++ mn-docker-lin-50-start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
-host=`hostname -i`
+sudo modprobe openvswitch
+host=`ifconfig docker0 |xargs|awk '{print $7}'|sed -e 's/[a-z]*:/''/'`
 docker run -it --privileged barbaracollignon/ubuntu-mininet /bin/bash -c "service openvswitch-switch start;mn --controller=remote,ip=$host,port=6633 --topo=linear,50 --switch=ovsk,protocols=OpenFlow13 --mac"
