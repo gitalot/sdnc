1aefa0fdf4d3a0935f4c31b2d30c01afee97901f
Kalidas Porika <kporika@gmail.com>
2016-03-19 10:19:18 -0400

    tree topolology with depth=4 and fanout=2

---
+++ mn-docker-tree-42-start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+host=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' sdnc`
+if [ -z "$host" ]; then
+    echo "Can't find ip for sdn container. Exit."
+    exit -1
+fi
+
+sudo modprobe openvswitch
+docker run -it --privileged barbaracollignon/ubuntu-mininet /bin/bash -c "service openvswitch-switch start;mn --controller=remote,ip=$host,port=6653 --topo=tree,depth=4,fanout=2 --switch=ovsk,protocols=OpenFlow13 --mac"
